455e4d014469ccaf50b2604fd62cdafbd47b4f3c
a <a@a.com>
2022-03-24 17:51:22 +0900

    a

--- src/main/java/spring/diary/service/TokenProvider.java
+++ src/main/java/spring/diary/service/TokenProvider.java
@@ -20,29 +20,31 @@ public class TokenProvider{
 	public String createToken(String subject) {
 		// 만료기한 = 현재시각 + 1일
 		Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.DAYS));
-		
-		/*
-			{ // header
-			  "alg":"HS512"
-			}.
-			{ // payload
-			  "sub":"40288093784915d201784916a40c0001",
-			  "iss": "diary",
-			  "iat":1595733657,
-			  "exp":1596597657
-			}.
-			// SECRET_KEY를 이용해 서명한 부분
-			Nn4d1MOVLZg79sfFACTIpCPKqWmpZMZQsbNrXdJJNWkRv50_l7bPLQPwhMobT4vBOG6Q3JYjhDrKFlBSaUxZOg
-		 */		
-		return Jwts.builder()
+		String token = Jwts.builder()
 				.signWith(key)
 				.setSubject(subject)
 				.setIssuer("diary")
 				.setIssuedAt(new Date())
 				.setExpiration(expiryDate)
 				.compact();
+		/* 토큰은 header.payload.signature 로 구성된다. 아래는 token 을 decoding 한 예이다.
+			{ // header
+			  "typ": "JWT"              : type
+			  "alg":"HS512"             : algorithm
+			}.
+			{ // payload
+			  "sub":"1",                : subject
+			  "iss": "diary",           : issuer
+			  "iat":1595733657,         : issued at
+			  "exp":1596597657          : expiration
+			}.			
+			Nn4d1MOVLZg79sfFACTIpCPKqWmpZMZQsbNrXdJJNWkRv50_l7bPLQPwhMobT4vBOG6Q3JYjhDrKFlBSaUxZOg : signature
+		*/
+		
+		return token;
 	}
 	
+	// user_id 를 subject 으로 사용한다.
 	public String validateToken(String token) {
 		Claims claims = Jwts.parser()
 				.setSigningKey(key)
