f406f8173838fb494e97bbd6bd3ff327b5f17cf3
a <a@a.com>
2022-03-29 16:17:10 +0900

    a

--- src/main/java/spring/diary/web/DiaryController.java
+++ src/main/java/spring/diary/web/DiaryController.java
@@ -3,6 +3,7 @@ package spring.diary.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.diary.domain.Todo;
@@ -33,18 +35,30 @@ public class DiaryController {
 	}
 	
 	@PostMapping("todo/add")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void addTodo(@RequestBody Todo todo, @AuthenticationPrincipal String userId) {
 		todo.setUserId(userId);
-		diaryService.addTodo(todo);
+		diaryService.addTodo(todo);		
 	}
 	
 	@PatchMapping("todo/fix")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void fixTodo(@RequestBody Todo todo) {		
 		diaryService.fixTodo(todo);
 	}
 	
 	@DeleteMapping("todo/del/{todoId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void delTodo(@PathVariable int todoId) {
 		diaryService.delTodo(todoId);
 	}	
 }
+
+/*
+@ResponseStatus(HttpStatus.NO_CONTENT) 에 대응해서,
+
+client 에선,
+if(response.ok && response.status != 204) {                 
+            return response.json()
+로 처리하면 된다.
+*/
