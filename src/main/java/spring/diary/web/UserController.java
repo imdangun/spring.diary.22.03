5f483e6ea601b90221eaece13c7d4e784f2b1c48
a <a@a.com>
2022-03-29 17:50:17 +0900

    a

--- src/main/java/spring/diary/web/UserController.java
+++ src/main/java/spring/diary/web/UserController.java
@@ -1,10 +1,12 @@
 package spring.diary.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import spring.diary.domain.User;
@@ -26,7 +28,8 @@ public class UserController {
 	}
 	
 	@PostMapping("signup")
-	public void signup(@RequestBody User user) {	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void signup(@RequestBody User user) {
 		userService.register(user);
 	}
 }
